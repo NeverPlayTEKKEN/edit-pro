51e254c3d5d582dc37b3ebd6013377d20b02baef
Naoki <gtvofromb@gmail.com>
2024-07-01 15:18:56 +0900

    こっみｔ

--- src/app/page.js
+++ src/app/page.js
@@ -10,30 +10,31 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div class="flex-grow" />
-      <OneMovie
-        image_src = '/background.png'
-        tittle = "その瞬間を逃しません"
-        description = {[<div class="flex justify-center">配信者人生で最高の瞬間はいつですか？</div>, <div class="flex justify-center"></div>]}
-      />
-      <FadeIn>
-      <div class="mx-2 md:mt-16 flex justify-start items-center">
-        <div class="h-[10px] w-[10px] mx-2 bg-red-600">　</div>
-        <div class="text-xl md:text-2xl xl:text-4xl">Plan</div>
-      </div>
+      <div>
+        <OneMovie
+          image_src = '/background.png'
+          tittle = "その瞬間、全世界へ"
+          description = {[<div class="flex justify-center">輝いている瞬間を切り抜き、新たな物語を紡ぎます</div>, <div class="flex justify-center"></div>]}
+        />
+        <FadeIn>
+          <div class="mx-2 md:mt-16 flex justify-start items-center">
+            <div class="h-[10px] w-[10px] mx-2 bg-red-600">　</div>
+            <div class="text-xl md:text-2xl xl:text-4xl">Plan</div>
+          </div>
       
-      <div class="grid grid-cols-3 mb-16">
-        <PlanBox description="梅のプランです"/>
-        <PlanBox description="竹のプランです"/>
-        <PlanBox description="松のプランです"/>
+          <div class="grid grid-cols-3 mb-16">
+            <PlanBox description="梅のプランです"/>
+            <PlanBox description="竹のプランです"/>
+            <PlanBox description="松のプランです"/>
+          </div>
+        </FadeIn>
+        <OneMovie
+          image_src = '/background.png'
+          tittle="二枚目の画像です"
+          description="説明します"
+        />
+        <Footer />
       </div>
-      </FadeIn>
-      <OneMovie
-        image_src = '/background.png'
-        tittle="二枚目の画像です"
-        description="説明します"
-      />
-      <Footer />
     </div>
   )
 }
