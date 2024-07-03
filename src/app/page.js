ff0f0f17b4b48bdba4ebe49ae75f910a350ab9d1
Naoki <gtvofromb@gmail.com>
2024-07-03 11:51:47 +0900

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ const Home = () => {
           description = {[<div class="flex justify-center">１７文字で改行されるので注意が必要</div>, <div class="flex justify-center"></div>]}
         />
         <FadeIn>
-          <div class="mx-2 md:mt-16 flex justify-start items-center">
+          <div class="mx-2 mt-4 md:mt-16 flex justify-start items-center">
             <div class="h-[10px] w-[10px] mx-2 bg-red-600">　</div>
             <div class="text-md md:text-2xl xl:text-4xl">Plan</div>
           </div>
@@ -27,6 +27,7 @@ const Home = () => {
             <PlanBox description="竹のプランです"/>
             <PlanBox description="松のプランです"/>
           </div>
+          
         </FadeIn>
         <OneMovie
           image_src = '/background.png'
