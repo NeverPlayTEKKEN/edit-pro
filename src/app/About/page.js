cf25e709cbb2ca31c5e6733afb747a3a8772660c
NaokiWindows <gtvofromb@gmail.com>
2024-10-15 19:55:42 +0900

    commit

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -9,9 +9,23 @@ const Contact = () => {
         <div>
             <Header />
             <OneMovie
-                image_src = '/me.webp'
-                tittle = "私はこんな人"
-                description = {[<div class="flex justify-center">１７文字で改行されるので注意が必要</div>, <div class="flex justify-center"></div>]}
+                image_src = '/ME.png'
+                description = {[<div class="flex justify-center">
+                    <div class="text-center mt-2 md:mt-0"><p>こんにちは！<br/>都内で動画編集をしているケントと申します！！</p>
+                    <br/>
+                    <p>お酒とアニソンとボーカロイドが大好きな30歳です！</p>
+                    <br/>
+  <p>Vtuber、ストリーマー、プロゲーマーの方々の<br></br>ゲーム実況や配信を日夜楽しんでいます！</p>
+  <br/>
+  <p>APEXやストリートファイター６等のゲームが好きで
+    <br/>さらに生放送も大好きなので</p>
+  <p>ライブ配信をもっと盛り上げたい！という人の<br/>お手伝いをするためにこのサービスを作りました！</p>
+  <br/>
+  <p>あなたが人気配信者になる一歩目の<br/>手助けができることに
+  わくわくしています！</p>
+  <br/>
+  <p><strong>ともにこの配信者大戦国時代を勝ち抜きましょう！</strong></p></div>
+                </div>, <div class="flex justify-center"></div>]}
             />
             <Footer />
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import OneMovie from "./components/OneMovie";
 import PlanBox from "./components/PlanBox";
 import Footer from "./components/Footer";
 import { FadeIn, FedIn } from './components/FadeIn'
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -17,15 +18,31 @@ const Home = () => {
           description = {[<div class="flex justify-center">いつもおもしろくはいられないから</div>, <div class="flex justify-center">あなたが輝いた瞬間を切り抜きます</div>]}
         />
         <FadeIn>
+        <div class="text-center md:text-base xl:text-xl bg-gray-200 text-sm py-4 mb-8 px-8 xl:py-12 xl:px-12">
+          <div class="bg-white p-2 xl:p-8" >
+          <p>配信をつけたらたくさんの視聴者で溢れ<br />
+          コメントが読み切れない…</p>
+          <br/>
+          <p>そんな未来を一緒に掴みましょう！</p>
+
+          <p>人気の配信者の<strong>“ほぼ全て”</strong>にあって<br />
+          貴方に無いもの…</p>
+          <br/>
+          <p>それは<strong class="bg-yellow-300">「切り抜き動画」</strong>です。</p>
+          <br/>
+          <p>ClipRiZeは切り抜き動画の制作を<br/>代行するサービスです。</p>
+        </div>
+        </div>
+        </FadeIn>
+        <FadeIn>
+
           <div class="mx-2 md:mt-16 md:ml-6 flex justify-start items-center">
             <div class="h-[10px] w-[10px] mx-2 bg-red-600">　</div>
             <div class="text-md md:text-2xl xl:text-4xl">Plan</div>
           </div>
       
-          <div class="grid grid-cols-3 md:mx-4">
-            <PlanBox description="梅のプランです"/>
-            <PlanBox description="竹のプランです"/>
-            <PlanBox description="松のプランです"/>
+          <div class="md:mx-4 relative text-center">
+            <Image src="/plan.PNG" width={1920} height={1080} objectFit="contain" />
           </div>
 
           <div class="mx-4 mt-2 mb-8 md:mb-16 flex justify-center text-sm md:text-base xl:text-xl  border-b-4 border-gray-100 md:border-none">
@@ -35,13 +52,24 @@ const Home = () => {
               <div class="w-fit md:border-b-4 border-gray-100">・お客様の満足のため全力疾走します！</div>
             </div>
           </div>
+        </FadeIn>
+        <FadeIn>
+
+          <div class="text-center bg-gray-200 text-sm  md:text-base xl:text-xl py-4 mb-8 px-8 xl:py-12 xl:px-12">
+          <div class="bg-white p-2 xl:p-8" >
+  <p class="text-slate-600">人が来ない…コメントが１…</p>
+  <p class="text-slate-600">誰かに見つけてほしい…</p>
+  <br/>
+  <p>YouTubeショートやTikTokは</p>
+  <p>再生数が上がりやすいので</p>
+  <p><strong class="bg-yellow-300">魅力が必ず伝わります！</strong></p>
+  <br/>
+  <p>あなたが人気配信者になる</p>
+  <p>お手伝いをさせてください！</p>
+</div>
+</div>
           
         </FadeIn>
-        <OneMovie
-          image_src = '/bg2.png'
-          tittle="二枚目の画像です"
-          description="説明します"
-        />
         <Footer />
       </div>
     </div>
