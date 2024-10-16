39e5e3a89fba35b97ecc780200d664415e0c13f8
NaokiWindows <gtvofromb@gmail.com>
2024-10-16 16:42:37 +0900

    commit

--- src/app/Service/page.js
+++ src/app/Service/page.js
@@ -9,19 +9,19 @@ const Service = () => {
         <div>
             <Header />
             <OneMovie
-                image_src = '/bg2.png'
-                tittle = "松のプラン"
-                description = {[<div class="flex justify-center">１７文字で改行されるので注意が必要</div>, <div class="flex justify-center"></div>]}
+                image_src = '/light.png'
+                tittle = "ライトプラン"
+                description = {[<div class="flex justify-center">お試しでご依頼いただく方におすすめ</div>, <div class="flex justify-center">再生数が伸びやすいショート動画を</div>,<div class="flex justify-center">ピンポイントで体験いただけます。</div>]}
             />
             <OneMovie
-                image_src = '/bg2.png'
-                tittle = "竹のプラン"
-                description = {[<div class="flex justify-center">１７文字で改行されるので注意が必要</div>, <div class="flex justify-center"></div>]}
+                image_src = '/standerd.png'
+                tittle = "スタンダードプラン"
+                description = {[<div class="flex justify-center">全部入った基本のプラン</div>, <div class="flex justify-center">はじめての方から継続の方まで</div>,<div class="flex justify-center">自信をもっておすすめできます。</div>]}
             />
             <OneMovie
-                image_src = '/bg2.png'
-                tittle = "梅のプラン"
-                description = {[<div class="flex justify-center">１７文字で改行されるので注意が必要</div>, <div class="flex justify-center"></div>]}
+                image_src = '/pro.png'
+                tittle = "プロプラン"
+                description = {[<div class="flex justify-center">本気で売れたい方におすすめ</div>, <div class="flex justify-center">最高のクオリティの動画を</div>, <div class="flex justify-center">最強の熱量で納品します！</div>]}
             />
             <Footer />
         </div>
